04a6354ab825779bde51c222fdbe135d4a416293
arghaghosh <argha7417@gmail.com>
2023-07-26 19:08:33 +0530

    EJS javascript added

--- app.js
+++ app.js
@@ -5,23 +5,44 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     var today = new Date();
+    var currentDay = today.getDay();
     var day = "";
 
-    if(today.getDay() == 6 || today.getDay() == 0)
-    {
-        day = "Weekend";
-    }
-    else
-    {
-        day = "Weekday";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thrusday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+
+        default:
+            console.log("Error Computer pagal ho gya");
     }
 
-    res.render("list", {kindofday: day});
+    res.render("list", {
+         kindofday: day
+        });
 
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(3000, function () {
+    console.log("Server started on port 3000.");
 });
