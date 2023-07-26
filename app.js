01f4967ce364ddbe0ff41b3820c0603170f46fa3
arghaghosh <argha7417@gmail.com>
2023-07-26 19:47:08 +0530

    List functionality

--- app.js
+++ app.js
@@ -3,45 +3,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items =[];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thrusday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
+    var options = {
+        weekday : "long",
+        day: "numeric",
+        month: "long"
+    };
 
-        default:
-            console.log("Error Computer pagal ho gya");
-    }
+    var day = today.toLocaleDateString("en-IN", options);
 
     res.render("list", {
-         kindofday: day
+         kindofday: day,
+         newlistitems: items
         });
 
 });
+
+app.post("/",function (req,res) {
+    var item = req.body.newitem;
+    items.push(item);
+    res.redirect("/");
+})
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
